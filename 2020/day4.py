79e3b8538660c54eaf5f1608a761fa6624e14eec
David Dai <davidwh.dai@gmail.com>
2020-12-06 19:31:31 -0500

    Completed day 4 part 2 :rocket:

--- 2020/day4.py
+++ 2020/day4.py
@@ -1,3 +1,6 @@
+import re
+
+
 def import_passports(filepath):
     """Given a filepath of passport data, return a list of dictionaries
     containing the passport data.
@@ -34,17 +37,53 @@ def import_passports(filepath):
             clean_passport[key] = val
 
         passports.append(clean_passport)
-        
+
     return passports
 
 
-def check_passport(passport, required_fields = ["byr",
-                                                "iyr",
-                                                "eyr", 
-                                                "hgt",
-                                                "hcl", 
-                                                "ecl",
-                                                "pid"]):
+def validate_data(passport):
+    """Given a passport, check that the data fields are all valid
+
+    1. 1920 <= byr <= 2002
+    2. 2010 <= iyr <= 2020
+    3. 2020 <= eyr <= 2030
+    4. 150cm/59in <= hgt <= 193cm/76in
+    5. hcl: a # followed by exactly six characters 0-9 or a-f
+    6. ecl: exactly one of: amb blu brn gry grn hzl oth
+    7. pid: a nine-digit number, including leading zeroes
+
+    Returns:
+        valid (boolean): True if all fields of passport are valid
+    """
+
+    byr = 1920 <= int(passport["byr"]) <= 2002
+    iyr = 2010 <= int(passport["iyr"]) <= 2020
+    eyr = 2020 <= int(passport["eyr"]) <= 2030
+
+    hgt_val = int(re.sub("[^0-9]", "", passport["hgt"]))
+    if "in" in passport["hgt"]:
+        hgt = 59 <= hgt_val <= 76
+    else:
+        hgt = 150 <= hgt_val <= 193
+
+    hcl = re.match("^#[0-9a-f]{6}$", passport["hcl"]) != None
+
+    ecl = passport["ecl"] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+    pid = re.match("^[0-9]{9}$", passport["pid"]) != None
+
+    valid = all([byr, iyr, eyr, hgt, hcl, ecl, pid])
+
+    return valid
+
+
+def check_passport_fields(passport, required_fields=["byr",
+                                                     "iyr",
+                                                     "eyr",
+                                                     "hgt",
+                                                     "hcl",
+                                                     "ecl",
+                                                     "pid"]):
     """Given a passport, check that it contains all the necessary fields. If so,
     it is considered valid, otherwise invalid.
 
@@ -54,15 +93,15 @@ def check_passport(passport, required_fields = ["byr",
     Returns:
         valid (boolean): Whether passport is valid or invalid
     """
-    
+
     keys = passport.keys()
-    
+
     valid = all([k in keys for k in required_fields])
-    
+
     return valid
 
 
-def count_valid(passports):
+def count_valid1(passports):
     """Given a list of passports, return the number of passports that are 
     considered valid.
 
@@ -72,14 +111,34 @@ def count_valid(passports):
     Returns:
         num_valid[int]: number of valid passports
     """
-    
+
     num_valid = 0
-    
+
     for passport in passports:
-        num_valid += check_passport(passport)    
-    
+        num_valid += check_passport_fields(passport)
+
+    return num_valid
+
+def count_valid2(passports):
+    """Given a list of passports, return the number of passports that are 
+    considered valid.
+
+    Args:
+        passports (list): list of dictionaries
+
+    Returns:
+        num_valid[int]: number of valid passports
+    """
+
+    num_valid = 0
+
+    for passport in passports:
+        if check_passport_fields(passport):
+            num_valid += validate_data(passport)
+
     return num_valid
 
 
 passports = import_passports("./day4_input.txt")
-print(count_valid(passports))
+print("Part1:", count_valid1(passports))
+print("Part2:", count_valid2(passports))

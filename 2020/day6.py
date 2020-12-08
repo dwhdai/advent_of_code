3e054fd5525b8eff135e0cc26c661c4f4dcfc5b5
David Dai <davidwh.dai@gmail.com>
2020-12-07 23:09:37 -0500

    Completed day 6 :fire:

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,3 +1,5 @@
+import string
+
 def import_responses(filepath):
     """Given a filepath, read in the text file containing the responses
     as a list object. Each element in the list is also a list object,
@@ -34,6 +36,25 @@ def count_yes(group):
     
     return count
 
+def count_all_yes(group):
+    """Given a group's responses, count the number of questions to which
+    EVERYONE answered "yes"
+
+    Args:
+        group (list): list object where each element is an individual's response
+
+    Returns:
+        count (int): number of questions where everyone answered "yes"
+    """
+    
+    count = 0
+    # initialize intersection to be all lowercase characters
+    all_yes = set(string.ascii_lowercase)
+    for response in group:
+        all_yes = all_yes.intersection(set(response))
+    
+    return len(all_yes)
+
 def group_count(responses):
     """Given a list of responses, for each group, count the number of questions
     to which anyone answered "yes".
@@ -49,6 +70,22 @@ def group_count(responses):
     
     return counts
 
+def group_all_count(responses):
+    """Given a list of responses, for each group, count the number of questions
+    to which everyone answered "yes".
+
+    Args:
+        responses (list): list of responses for all groups
+
+    Returns:
+        counts (list): number of "yes" questions for each group
+    """
+    
+    counts = [count_all_yes(group) for group in responses]
+    
+    return counts
+
 if __name__ == "__main__":
     responses = import_responses("./day6_input.txt")
     print("Part 1:", sum(group_count(responses)))
+    print("Part 2:", sum(group_all_count(responses)))

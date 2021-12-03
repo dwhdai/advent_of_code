c695f0e5d6ddf3f9629b2def50d6086d81372586
David Dai <davidwh.dai@gmail.com>
2021-12-03 00:09:10 -0500

    2021 Day2

--- 2021/day2.py
+++ 2021/day2.py
@@ -4,7 +4,34 @@ with open("2021/day2_input.txt", "r") as f:
 
 # Change up steps to negative
 instructions = [(dir, -steps) if dir == "up" else (dir, steps) for (dir, steps) in instructions]
-horizontal = [steps for (dir, steps) in instructions if dir=="forward"]
-vertical = [steps for (dir, steps) in instructions if dir in ["up", "down"]]
-answer = sum(horizontal) * sum(vertical)
-print(answer)
+
+def part1(instructions):
+    horizontal = [steps for (dir, steps) in instructions if dir=="forward"]
+    vertical = [steps for (dir, steps) in instructions if dir in ["up", "down"]]
+    answer = sum(horizontal) * sum(vertical)
+    return answer
+
+
+def part2(instructions):
+
+    horizontal = [steps for (dir, steps) in instructions if dir=="forward"]
+    aim = []
+    for i, (dir, a) in enumerate(instructions):
+        if i == 0 and dir in ["up", "down"]:
+            aim.append(a)
+        elif i == 0 and dir == "forward":
+            aim.append(0)
+        elif dir in ["forward"]:
+            aim.append(aim[i-1])
+        else:
+            a = a + aim[i-1]
+            aim.append(a)
+
+    vertical = [aim*steps for (aim, (dir, steps)) in zip(aim, instructions) if dir=="forward"]
+    answer = sum(horizontal) * sum(vertical)
+    return answer
+
+print(part1(instructions))
+print(part2(instructions))
+
+

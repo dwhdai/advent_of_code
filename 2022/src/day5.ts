e55a1ad149ed7023f721adb6e3a609a5dc9c7236
David Dai <david.dai@ada.support>
2022-12-05 11:26:38 -0500

    create day 5 template

---
+++ 2022/src/day5.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+
+const dataPath = 'data/day5.txt'
+const rawData = fs.readFileSync(dataPath, 'utf8').split("\n")
+
+
+export function part1(): number {
+    return 1
+}
+
+export function part2(): number {
+    return 1
+}
